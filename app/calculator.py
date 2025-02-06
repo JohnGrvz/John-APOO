30d3961726df3b4189377890a4b8755ab5a1e297
JohnG <johnalejandrogaravito@gmail.com>
2025-02-06 13:14:20 -0500

    Implemented divide method

--- app/calculator.py
+++ app/calculator.py
@@ -3,5 +3,7 @@ class Calculator:
         return a + b
     def subtract(self,a,b):
         return  a - b
-    def multiply (self,a,b):
+    def multiply(self,a,b):
         return a*b
+    def divide(self,a,b):
+        return a/ b
